50b2e26399ea520cd8136b3f704a25318bd64b2a
WANGKAIREN <kairen.wang@qq.com>
2023-09-06 00:03:29 +0800

    generoi/sage-woocommerce 使用测试

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,19 @@
 
 @include('sections.header')
 
+{{-- 测试 generoi/sage-woocommerce start --}}
+{{--@include('archive-product')--}}
+{{--@include('woocommerce.single-product')--}}
+
+@php
+  defined( 'ABSPATH' ) || exit;
+@endphp
+@php
+  wc_get_template( 'archive-product.php' );
+@endphp
+
+{{-- 测试 generoi/sage-woocommerce end --}}
+
 {{--   <main id="main" class="main"> --}}
 {{--     @yield('content') --}}
 {{--   </main> --}}
